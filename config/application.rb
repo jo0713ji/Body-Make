6c81517987d34514157a0fac3e2887270829318c
jo0713ji <so0627suke@gmail.com>
2023-03-03 17:00:38 +0900

    Add:rails g時にhelperファイルとassetsファイルが自動作成されないように設定追加

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module BodyMake
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
