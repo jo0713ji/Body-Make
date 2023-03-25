bfe04c1eafce2eae491ec5c35708ae0c2d2fbccc
jo0713ji <so0627suke@gmail.com>
2023-03-26 06:34:36 +0900

    Add:daisyUIの導入

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,5 +20,6 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/container-queries'),
+    require("daisyui"),
   ]
 }
