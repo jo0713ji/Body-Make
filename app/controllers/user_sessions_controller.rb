da295868ed2ec0d93fad4c9ffb34d29ad49b2e44
jo0713ji <so0627suke@gmail.com>
2023-03-31 18:05:56 +0900

    Add:user_sessionsコントローラ作成・処理実装

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login
+  
+  def new; end
+
+  def create
+    @user = login(params[:name],params[:email],params[:password])
+
+    if @user
+      redirect_back_or_to root_path, success: t('.success')
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, success: t('.success')
+  end
+end
