d404f4d5a1794ff2e81eccc0362de4d28b27f854
jo0713ji <so0627suke@gmail.com>
2023-03-31 18:04:58 +0900

    Add:user_sessionsのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   # Defines the root path route ("/")
   root "tops#index"
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
