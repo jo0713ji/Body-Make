7e29ca4cd12000a1e3a45809ef580748b3df3cd9
jo0713ji <so0627suke@gmail.com>
2023-04-07 05:57:07 +0900

    Add:ログイン機能実装の為、コントローラ作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,10 +1,8 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login
-  
   def new; end
 
   def create
-    @user = login(params[:name],params[:email],params[:password])
+    @user = login(params[:email],params[:password])
 
     if @user
       redirect_back_or_to root_path, success: t('.success')
