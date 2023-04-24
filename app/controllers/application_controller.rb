6d37a1cc655c08c3ce040c355ed2a731142ab6a6
jo0713ji <so0627suke@gmail.com>
2023-04-24 16:26:12 +0900

    Add:flashメッセージ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # フラッシュメッセージの種類を増やす
+  add_flash_types :success, :info, :warning, :error
+  before_action :require_login
+
+  private
+
+  # ログインしていない場合はログイン画面にリダイレクトする
+  def not_authenticated
+    redirect_to login_path, error: t('defaults.please_login_first')
+  end
+
+  # ログインしている場合はダッシュボードにリダイレクトする
+  def require_not_logged_in
+    redirect_to dashboards_path, error: t('defaults.invalid_access') if logged_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+  
   def new
     @user = User.new
   end
@@ -6,10 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: 'ユーザー登録しました！'
+      redirect_to login_path, success: t('.success')
     else
-      flash.now[:danger] = 'ユーザー作成に失敗しました'
-      render :new
+      flash.now[:error] = t('.fail')
+      render :new, status: :unprocessable_entity
     end
   end
 
